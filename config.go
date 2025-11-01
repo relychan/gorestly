b9758c1590e72d75921c50ff3e460578508d2f7f
Toan Nguyen <hgiasac@gmail.com>
2025-11-02 00:30:21 +0700

    Change the naming convention to camelCase (#1)

--- config.go
+++ config.go
@@ -13,10 +13,14 @@ import (
 
 // RestyConfig contains configurations to create client.
 type RestyConfig struct {
-	Timeout   *model.Duration      `json:"timeout,omitempty"   jsonschema:"nullable,type=string,pattern=^((([0-9]+h)?([0-9]+m)?([0-9]+s))|(([0-9]+h)?([0-9]+m))|([0-9]+h))$" mapstructure:"timeout"     yaml:"timeout,omitempty"`
-	Transport *HTTPTransportConfig `json:"transport,omitempty" mapstructure:"transport"                                                                                      yaml:"transport,omitempty"`
-	TLS       *TLSConfig           `json:"tls,omitempty"       mapstructure:"tls"                                                                                            yaml:"tls,omitempty"`
-	Retry     *RestyRetryConfig    `json:"retry,omitempty"     mapstructure:"retry"                                                                                          yaml:"retry,omitempty"`
+	// Default maximum timeout duration that is applied for all requests.
+	Timeout *model.Duration `json:"timeout,omitempty" jsonschema:"nullable,type=string,pattern=^((([0-9]+h)?([0-9]+m)?([0-9]+s))|(([0-9]+h)?([0-9]+m))|([0-9]+h))$" mapstructure:"timeout" yaml:"timeout,omitempty"`
+	// Transport stores the http.Transport configuration for the http client.
+	Transport *HTTPTransportConfig `json:"transport,omitempty" mapstructure:"transport" yaml:"transport,omitempty"`
+	// The transport layer security (LTS) configuration for the mutualTLS authentication.
+	TLS *TLSConfig `json:"tls,omitempty" mapstructure:"tls" yaml:"tls,omitempty"`
+	// Retry policy of client requests.
+	Retry *RestyRetryConfig `json:"retry,omitempty" mapstructure:"retry" yaml:"retry,omitempty"`
 }
 
 // ToTransport creates an http transport from configurations.
@@ -50,54 +54,54 @@ type RestyDialerConfig struct {
 	// If Deadline is also set, it may fail earlier.
 	Timeout *model.Duration `json:"timeout,omitempty" jsonschema:"nullable,type=string,pattern=^((([0-9]+h)?([0-9]+m)?([0-9]+s))|(([0-9]+h)?([0-9]+m))|([0-9]+h))$" mapstructure:"timeout" yaml:"timeout"`
 	// Keep-alive probes are enabled by default.
-	KeepAliveEnabled *bool `json:"keep_alive_enabled,omitempty" mapstructure:"keep_alive_enabled" yaml:"keep_alive_enabled"`
-	// The time between keep-alive probes. If zero, a default value of 15 seconds is used.
-	KeepAliveInterval *model.Duration `json:"keep_alive_interval,omitempty" jsonschema:"nullable,type=string,pattern=^((([0-9]+h)?([0-9]+m)?([0-9]+s))|(([0-9]+h)?([0-9]+m))|([0-9]+h))$" mapstructure:"keep_alive_interval" yaml:"keep_alive_interval"`
-	// The maximum number of keep-alive probes that can go unanswered before dropping a connection.
+	KeepAliveEnabled *bool `json:"keepAliveEnabled,omitempty" mapstructure:"keepAliveEnabled" yaml:"keepAliveEnabled"`
+	// KeepAliveInterval is the time between keep-alive probes. If zero, a default value of 15 seconds is used.
+	KeepAliveInterval *model.Duration `json:"keepAliveInterval,omitempty" jsonschema:"nullable,type=string,pattern=^((([0-9]+h)?([0-9]+m)?([0-9]+s))|(([0-9]+h)?([0-9]+m))|([0-9]+h))$" mapstructure:"keepAliveInterval" yaml:"keepAliveInterval"`
+	// KeepAliveCount is the maximum number of keep-alive probes that can go unanswered before dropping a connection.
 	// If zero, a default value of 9 is used.
-	KeepAliveCount *int `json:"keep_alive_count,omitempty" jsonschema:"nullable,min=0" mapstructure:"keep_alive_count" yaml:"keep_alive_count"`
-	// The time that the connection must be idle before the first keep-alive probe is sent.
+	KeepAliveCount *int `json:"keepAliveCount,omitempty" jsonschema:"nullable,min=0" mapstructure:"keepAliveCount" yaml:"keepAliveCount"`
+	// KeepAliveIdle is the time that the connection must be idle before the first keep-alive probe is sent.
 	// If zero, a default value of 15 seconds is used.
-	KeepAliveIdle *model.Duration `json:"keep_alive_idle,omitempty" jsonschema:"nullable,type=string,pattern=^((([0-9]+h)?([0-9]+m)?([0-9]+s))|(([0-9]+h)?([0-9]+m))|([0-9]+h))$" mapstructure:"keep_alive_idle" yaml:"keep_alive_idle"`
+	KeepAliveIdle *model.Duration `json:"keepAliveIdle,omitempty" jsonschema:"nullable,type=string,pattern=^((([0-9]+h)?([0-9]+m)?([0-9]+s))|(([0-9]+h)?([0-9]+m))|([0-9]+h))$" mapstructure:"keepAliveIdle" yaml:"keepAliveIdle"`
 	// FallbackDelay specifies the length of time to wait before spawning a RFC 6555 Fast Fallback connection.
 	// That is, this is the amount of time to wait for IPv6 to succeed before assuming that IPv6 is misconfigured and falling back to IPv4.
 	// If zero, a default delay of 300ms is used. A negative value disables Fast Fallback support.
-	FallbackDelay *model.Duration `json:"fallback_delay,omitempty" jsonschema:"nullable,type=string,pattern=^((([0-9]+h)?([0-9]+m)?([0-9]+s))|(([0-9]+h)?([0-9]+m))|([0-9]+h))$" mapstructure:"fallback_delay" yaml:"fallback_delay"`
+	FallbackDelay *model.Duration `json:"fallbackDelay,omitempty" jsonschema:"nullable,type=string,pattern=^((([0-9]+h)?([0-9]+m)?([0-9]+s))|(([0-9]+h)?([0-9]+m))|([0-9]+h))$" mapstructure:"fallbackDelay" yaml:"fallbackDelay"`
 }
 
 // HTTPTransportConfig stores the http.Transport configuration for the http client.
 type HTTPTransportConfig struct {
 	// Options the http.Dialer to connect to an address
 	Dialer *RestyDialerConfig `json:"dialer,omitempty" mapstructure:"dialer" yaml:"dialer"`
-	// The maximum amount of time an idle (keep-alive) connection will remain idle before closing itself. Zero means no limit.
-	IdleConnTimeout *model.Duration `json:"idle_conn_timeout,omitempty" jsonschema:"nullable,type=string,pattern=^((([0-9]+h)?([0-9]+m)?([0-9]+s))|(([0-9]+h)?([0-9]+m))|([0-9]+h))$" mapstructure:"idle_conn_timeout" yaml:"idle_conn_timeout"`
-	// If non-zero, specifies the amount of time to wait for a server's response headers after fully writing the request (including its body, if any).
+	// Idle connection timeout. The maximum amount of time an idle (keep-alive) connection will remain idle before closing itself. Zero means no limit.
+	IdleConnTimeout *model.Duration `json:"idleConnTimeout,omitempty" jsonschema:"nullable,type=string,pattern=^((([0-9]+h)?([0-9]+m)?([0-9]+s))|(([0-9]+h)?([0-9]+m))|([0-9]+h))$" mapstructure:"idleConnTimeout" yaml:"idleConnTimeout"`
+	// Response header timeout, if non-zero, specifies the amount of time to wait for a server's response headers after fully writing the request (including its body, if any).
 	// This time does not include the time to read the response body.
 	// This timeout is used to cover cases where the tcp connection works but the server never answers.
-	ResponseHeaderTimeout *model.Duration `json:"response_header_timeout,omitempty" jsonschema:"nullable,type=string,pattern=^((([0-9]+h)?([0-9]+m)?([0-9]+s))|(([0-9]+h)?([0-9]+m))|([0-9]+h))$" mapstructure:"response_header_timeout" yaml:"response_header_timeout"`
-	// The maximum amount of time to wait for a TLS handshake. Zero means no timeout.
-	TLSHandshakeTimeout *model.Duration `json:"tls_handshake_timeout,omitempty" jsonschema:"nullable,type=string,pattern=^((([0-9]+h)?([0-9]+m)?([0-9]+s))|(([0-9]+h)?([0-9]+m))|([0-9]+h))$" mapstructure:"tls_handshake_timeout" yaml:"tls_handshake_timeout"`
-	// If non-zero, specifies the amount of time to wait for a server's first response headers after fully writing the request headers if the request has an "Expect: 100-continue" header.
-	ExpectContinueTimeout *model.Duration `json:"expect_continue_timeout,omitempty" jsonschema:"nullable,type=string,pattern=^((([0-9]+h)?([0-9]+m)?([0-9]+s))|(([0-9]+h)?([0-9]+m))|([0-9]+h))$" mapstructure:"expect_continue_timeout" yaml:"expect_continue_timeout"`
+	ResponseHeaderTimeout *model.Duration `json:"responseHeaderTimeout,omitempty" jsonschema:"nullable,type=string,pattern=^((([0-9]+h)?([0-9]+m)?([0-9]+s))|(([0-9]+h)?([0-9]+m))|([0-9]+h))$" mapstructure:"responseHeaderTimeout" yaml:"responseHeaderTimeout"`
+	// TLS handshake timeout is the maximum amount of time to wait for a TLS handshake. Zero means no timeout.
+	TLSHandshakeTimeout *model.Duration `json:"tlsHandshakeTimeout,omitempty" jsonschema:"nullable,type=string,pattern=^((([0-9]+h)?([0-9]+m)?([0-9]+s))|(([0-9]+h)?([0-9]+m))|([0-9]+h))$" mapstructure:"tlsHandshakeTimeout" yaml:"tlsHandshakeTimeout"`
+	// Expect continue timeout, if non-zero, specifies the amount of time to wait for a server's first response headers after fully writing the request headers if the request has an "Expect: 100-continue" header.
+	ExpectContinueTimeout *model.Duration `json:"expectContinueTimeout,omitempty" jsonschema:"nullable,type=string,pattern=^((([0-9]+h)?([0-9]+m)?([0-9]+s))|(([0-9]+h)?([0-9]+m))|([0-9]+h))$" mapstructure:"expectContinueTimeout" yaml:"expectContinueTimeout"`
 	// MaxIdleConns controls the maximum number of idle (keep-alive) connections across all hosts. Zero means no limit.
-	MaxIdleConns *int `json:"max_idle_conns,omitempty" jsonschema:"nullable,min=0" mapstructure:"max_idle_conns" yaml:"max_idle_conns"`
+	MaxIdleConns *int `json:"maxIdleConns,omitempty" jsonschema:"nullable,min=0" mapstructure:"maxIdleConns" yaml:"maxIdleConns"`
 	// MaxIdleConnsPerHost, if non-zero, controls the maximum idle (keep-alive) connections to keep per-host.
-	MaxIdleConnsPerHost *int `json:"max_idle_conns_per_host,omitempty" jsonschema:"nullable,min=0" mapstructure:"max_idle_conns_per_host" yaml:"max_idle_conns_per_host"`
-	// Optionally limits the total number of connections per host, including connections in the dialing, active, and idle states.
+	MaxIdleConnsPerHost *int `json:"maxIdleConnsPerHost,omitempty" jsonschema:"nullable,min=0" mapstructure:"maxIdleConnsPerHost" yaml:"maxIdleConnsPerHost"`
+	// MaxConnsPerHost optionally limits the total number of connections per host, including connections in the dialing, active, and idle states.
 	// On limit violation, dials will block. Zero means no limit.
-	MaxConnsPerHost *int `json:"max_conns_per_host,omitempty" jsonschema:"nullable,min=0" mapstructure:"max_conns_per_host" yaml:"max_conns_per_host"`
-	// Specifies a limit on how many response bytes are allowed in the server's response header.
+	MaxConnsPerHost *int `json:"maxConnsPerHost,omitempty" jsonschema:"nullable,min=0" mapstructure:"maxConnsPerHost" yaml:"maxConnsPerHost"`
+	// MaxResponseHeaderBytes specifies a limit on how many response bytes are allowed in the server's response header.
 	// Zero means to use a default limit.
-	MaxResponseHeaderBytes *int64 `json:"max_response_header_bytes,omitempty" jsonschema:"nullable,min=0" mapstructure:"max_response_header_bytes" yaml:"max_response_header_bytes"`
+	MaxResponseHeaderBytes *int64 `json:"maxResponseHeaderBytes,omitempty" jsonschema:"nullable,min=0" mapstructure:"maxResponseHeaderBytes" yaml:"maxResponseHeaderBytes"`
 	// ReadBufferSize specifies the size of the read buffer used when reading from the transport.
 	// If zero, a default (currently 4KB) is used.
-	ReadBufferSize *int `json:"read_buffer_size,omitempty" jsonschema:"nullable,min=0" mapstructure:"read_buffer_size" yaml:"read_buffer_size"`
+	ReadBufferSize *int `json:"readBufferSize,omitempty" jsonschema:"nullable,min=0" mapstructure:"readBufferSize" yaml:"readBufferSize"`
 	// WriteBufferSize specifies the size of the write buffer used when writing to the transport.
 	// If zero, a default (currently 4KB) is used.
-	WriteBufferSize *int `json:"write_buffer_size,omitempty" jsonschema:"nullable,min=0" mapstructure:"write_buffer_size" yaml:"write_buffer_size"`
+	WriteBufferSize *int `json:"writeBufferSize,omitempty" jsonschema:"nullable,min=0" mapstructure:"writeBufferSize" yaml:"writeBufferSize"`
 	// DisableKeepAlives, if true, disables HTTP keep-alives and will only use the connection to the server for a single HTTP request.
 	// This is unrelated to the similarly named TCP keep-alives.
-	DisableKeepAlives bool `json:"disable_keep_alives,omitempty" mapstructure:"disable_keep_alives" yaml:"disable_keep_alives"`
+	DisableKeepAlives bool `json:"disableKeepAlives,omitempty" mapstructure:"disableKeepAlives" yaml:"disableKeepAlives"`
 }
 
 // ToTransport creates an http transport from the configuration.
